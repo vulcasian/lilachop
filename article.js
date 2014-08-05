0603bf5b14adcb4d969ec90d105c1dabebb2c663
kristaps <>
2014-08-05 13:23:55 +0000

    Make sure that servings is really a number.

--- article.js
+++ article.js
@@ -1,9 +1,13 @@
 
 function changeservings(e)
 {
-	var elems = getAllElementsWithAttribute('data-lilachop-amount');
+	var elems;
 	var num = parseFloat(e);
 
+	if (isNaN(num))
+		return;
+
+	elems = getAllElementsWithAttribute('data-lilachop-amount');
 	for (var i = 0, n = elems.length; i < n; i++)
 		elems[i].innerHTML = num * parseFloat(elems[i].getAttribute('data-lilachop-amount'));
 }
@@ -12,8 +16,10 @@ function getAllElementsWithAttribute(attribute)
 {
 	var matchingElements = [];
 	var allElements = document.getElementsByTagName('*');
+
 	for (var i = 0, n = allElements.length; i < n; i++) 
 		if (allElements[i].getAttribute(attribute))
 			matchingElements.push(allElements[i]);
+
 	return matchingElements;
 }
